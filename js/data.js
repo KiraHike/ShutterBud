691dd882dc8b3476425b391e3b8f32c197367eb5
KiraHike <kirahikes@gmail.com>
2021-02-23 17:51:58 -0800

    Add JS function to request astro data from API and add to data model #2

--- js/data.js
+++ js/data.js
@@ -1,4 +1,14 @@
-/* exported data */
+/* exported astroData */
+/* exported weatherData */
 
-var astroData = {};
+var astroData = {
+  zip: null,
+  sunrise: '00:00',
+  solarNoon: '00:00',
+  sunset: '00:00',
+  moonrise: '00:00',
+  moonset: '00:00',
+  dayLength: '00:00'
+};
+
 var weatherData = {};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+/* global astroData */
+/* global weatherData */
+
 var $pageLanding = document.querySelector('#page-landing');
 var $pagePlan = document.querySelector('#page-plan');
-
 var $header = document.querySelector('h2');
-
+var $zipInputPlan = document.querySelector('#zip-plan');
 var $navPlan = document.querySelector('.nav.plan');
 
 function viewPlan(event) {
@@ -12,4 +14,28 @@ function viewPlan(event) {
   $header.textContent = 'Location';
 }
 
+function getAstroData(event) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.ipgeolocation.io/astronomy?apiKey=e79aa15752144199b62b2a82488d2761&location='
+    + astroData.zip);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    astroData.sunrise = xhr.response.sunrise;
+    astroData.solarNoon = xhr.response.solar_noon;
+    astroData.sunset = xhr.response.sunset;
+    astroData.moonrise = xhr.response.moonrise;
+    astroData.moonset = xhr.response.moonset;
+    astroData.dayLength = xhr.response.day_length;
+  });
+  xhr.send();
+}
+
 $navPlan.addEventListener('click', viewPlan);
+
+$zipInputPlan.addEventListener('input', function() {
+  if ($zipInputPlan.value.length < 5) {
+    return;
+  }
+  astroData.zip = $zipInputPlan.value;
+  getAstroData(event);
+})
