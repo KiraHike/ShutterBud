bb717e80e697d671ac9be97ed37b316a826db59a
KiraHike <kirahikes@gmail.com>
2021-02-23 18:12:25 -0800

    Add JS function to request weather data from API and add to data model #2

--- js/data.js
+++ js/data.js
@@ -11,4 +11,14 @@ var astroData = {
   dayLength: '00:00'
 };
 
-var weatherData = {};
+var weatherData = {
+  zip: null,
+  temperature: '0',
+  humidity: '0',
+  uvIndex: '0',
+  windDirection: 'N',
+  feelsLike: '0',
+  precipitation: '0',
+  cloudCover: '0',
+  windSpeed: '0'
+};
--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ function viewPlan(event) {
 
 function getAstroData(event) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.ipgeolocation.io/astronomy?apiKey=e79aa15752144199b62b2a82488d2761&location='
-    + astroData.zip);
+  xhr.open('GET', 'https://api.ipgeolocation.io/astronomy?apiKey=e79aa15752144199b62b2a82488d2761&location=' +
+    astroData.zip);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     astroData.sunrise = xhr.response.sunrise;
@@ -30,12 +30,32 @@ function getAstroData(event) {
   xhr.send();
 }
 
+function getWeatherData(event) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://api.weatherstack.com/current?access_key=dcb9c9ee82f6655c925b30ce5386c0d2&query=' +
+    weatherData.zip);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    weatherData.temperature = xhr.response.current.temperature;
+    weatherData.humidity = xhr.response.current.humidity;
+    weatherData.uvIndex = xhr.response.current.uv_index;
+    weatherData.windDirection = xhr.response.current.wind_dir;
+    weatherData.feelsLike = xhr.response.current.feelslike;
+    weatherData.precipitation = xhr.response.current.precip;
+    weatherData.cloudCover = xhr.response.current.cloudcover;
+    weatherData.windSpeed = xhr.response.current.wind_speed;
+  });
+  xhr.send();
+}
+
 $navPlan.addEventListener('click', viewPlan);
 
-$zipInputPlan.addEventListener('input', function() {
+$zipInputPlan.addEventListener('input', function () {
   if ($zipInputPlan.value.length < 5) {
     return;
   }
   astroData.zip = $zipInputPlan.value;
+  weatherData.zip = $zipInputPlan.value;
   getAstroData(event);
-})
+  getWeatherData(event);
+});
